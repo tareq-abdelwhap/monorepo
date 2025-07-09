e89d5c6abbd91b0dbb1cbf148296a9760d31f58e
Tareq Abdelwhap <tarekian99@gmail.com>
2025-07-09 14:44:48 +0300

    try: something else

--- apps/new-portfolio/netlify.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteId: process.env.NETLIFY_SITE_ID_PORTFOLIO_V2,
-  buildCommand: 'pnpm run build',
-  dist: '.output/public',
-  build: {
-    publish: '.output/public',
-  },
-};
