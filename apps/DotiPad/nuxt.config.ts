72eb2eaa149e2ef48d77a8cb33ea67a8eadf82f4
Tareq Abdelwhap <tarekian99@gmail.com>
2025-07-09 16:18:27 +0300

    fix: i18n package in doti-pad

--- apps/DotiPad/nuxt.config.ts
+++ apps/DotiPad/nuxt.config.ts
@@ -4,33 +4,34 @@ export default defineNuxtConfig({
 
   nitro: { preset: 'netlify' },
 
-  modules: ['nuxt-starter', '@vueuse/motion/nuxt'],
+  modules: ['nuxt-starter', '@vueuse/motion/nuxt', '@nuxtjs/i18n'],
 
   starter: {
     googleFonts: {
       families: { Nunito: [400, 600, 700] },
     },
-    i18n: {
-      baseUrl: 'http://localhost:3000',
-      locales: [
-        {
-          code: 'en',
-          language: 'en-US',
-          name: 'English',
-          file: 'en.ts',
-          dir: 'ltr',
-        },
-        {
-          code: 'ar',
-          language: 'ar-EG',
-          name: 'العربية',
-          file: 'ar.ts',
-          dir: 'rtl',
-        },
-      ],
-      defaultLocale: 'en',
-      strategy: 'prefix_except_default',
-      lazy: true,
-    },
+  },
+
+  i18n: {
+    baseUrl: 'http://localhost:3000',
+    locales: [
+      {
+        code: 'en',
+        language: 'en-US',
+        name: 'English',
+        file: 'en.ts',
+        dir: 'ltr',
+      },
+      {
+        code: 'ar',
+        language: 'ar-EG',
+        name: 'العربية',
+        file: 'ar.ts',
+        dir: 'rtl',
+      },
+    ],
+    defaultLocale: 'en',
+    strategy: 'prefix_except_default',
+    lazy: true,
   },
 });
