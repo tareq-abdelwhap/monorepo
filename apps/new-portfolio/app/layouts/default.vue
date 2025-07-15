fb4764481115004f93c8c49ee71b68baf417c15d
Tareq Abdelwhap <tarekian99@gmail.com>
2025-07-15 16:25:41 +0300

    feat: add footer and link it to my linked-in profile

--- apps/new-portfolio/app/layouts/default.vue
+++ apps/new-portfolio/app/layouts/default.vue
@@ -4,9 +4,21 @@
   <div ref="container" class="relative h-screen w-full overflow-hidden">
     <BasePreLoader />
 
-    <BaseLoginButton />
+    <BaseCreateYoursButton />
 
     <slot />
+
+    <span
+      class="absolute bottom-0 end-0 z-[150] text-white sm:text-sm px-8 py-4"
+    >
+      made with 🤍 by
+      <a
+        class="font-bold underline hover:text-blue-500 text-blue-300"
+        href="https://www.linkedin.com/in/tareq-abdelwhap/"
+        target="_blank"
+        v-text="`Tareq Abdelwhap`"
+      />
+    </span>
   </div>
 </template>
 
