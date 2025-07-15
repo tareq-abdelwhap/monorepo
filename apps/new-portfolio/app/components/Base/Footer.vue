64cfaad0611a68016193a7b81910fb4afe1b2f94
Tareq Abdelwhap <tarekian99@gmail.com>
2025-07-15 17:19:41 +0300

    feat: Footer component and auth Layout

---
+++ apps/new-portfolio/app/components/Base/Footer.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+
+<template>
+  <span class="absolute bottom-0 end-0 z-[150] text-white sm:text-sm px-8 py-4">
+    made with 🤍 by
+    <a
+      class="font-bold underline hover:text-blue-500 text-blue-300"
+      href="https://www.linkedin.com/in/tareq-abdelwhap/"
+      target="_blank"
+      v-text="`Tareq Abdelwhap`"
+    />
+  </span>
+</template>
+
+<style scoped></style>
---
+++ apps/new-portfolio/app/layouts/auth.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    :class="[
+      'relative h-screen w-full overflow-hidden',
+      'flex justify-center items-center',
+      'bg-black text-white',
+    ]"
+  >
+    <slot />
+
+    <BaseFooter />
+  </div>
+</template>
+
+<style scoped></style>
