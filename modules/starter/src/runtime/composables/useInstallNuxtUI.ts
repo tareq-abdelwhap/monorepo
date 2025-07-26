bc172619d75490d845c5f14320ee6ebe2ef0b904
Tareq Abdelwhap <tarekian99@gmail.com>
2025-07-26 10:14:07 +0300

    ref: make alias for ui.css file from the custom module

--- modules/starter/src/runtime/composables/useInstallNuxtUI.ts
+++ modules/starter/src/runtime/composables/useInstallNuxtUI.ts
@@ -14,8 +14,8 @@ export const useInstallNuxtUI = async (
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
 
-  // nuxt.options.alias ||= {};
-  // nuxt.options.alias["@nuxt-starter/ui"] = resolve(__dirname, "./../ui.css");
+  nuxt.options.alias ||= {};
+  nuxt.options.alias["@nuxt-starter/ui"] = resolve(__dirname, "./../ui.css");
 
   // check if file exists and if not create it with createFileSync
 
@@ -24,8 +24,8 @@ export const useInstallNuxtUI = async (
   // check if file exists
   if (!fs.existsSync(cssPath)) {
     // create the file with the default content
-    // fs.writeFileSync(cssPath, "@import '@nuxt-starter/ui';\n");
-    fs.writeFileSync(cssPath, "@import 'tailwindcss';\n@import '@nuxt/ui';\n");
+    fs.writeFileSync(cssPath, "@import '@nuxt-starter/ui';\n");
+    // fs.writeFileSync(cssPath, "@import 'tailwindcss';\n@import '@nuxt/ui';\n");
 
     console.log(`Created ${cssPath} file. Please add your NuxtUI Config here.`);
   }
